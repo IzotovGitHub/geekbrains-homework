5ceb7ce7fd4af8ed276ae0a7284114d7413031d3
izotov <aleksandr.izotov2020@gmail.com>
2022-03-01 20:22:53 +0300

    Added isSumBetweenTenAndTwenty method

--- src/geek/brains/quarter/one/java1/lession2/HomeWorkApp.java
+++ src/geek/brains/quarter/one/java1/lession2/HomeWorkApp.java
@@ -5,4 +5,9 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
     }
+
+    private static boolean isSumBetweenTenAndTwenty(int first, int second) {
+        int sum = first + second;
+        return sum >= 10 && sum <= 20;
+    }
 }
