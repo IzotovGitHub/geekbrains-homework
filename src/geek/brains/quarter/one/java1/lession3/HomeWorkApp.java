7ca5617c8b03e2ca275e045e0dfa430ba91dd495
izotov <aleksandr.izotov2020@gmail.com>
2022-03-03 21:18:20 +0300

    Added replaceElements method

--- src/geek/brains/quarter/one/java1/lession3/HomeWorkApp.java
+++ src/geek/brains/quarter/one/java1/lession3/HomeWorkApp.java
@@ -2,4 +2,10 @@ package geek.brains.quarter.one.java1.lession3;
 
 public class HomeWorkApp {
 
+    private static int[] replaceElements(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] == 0 ? 1 : 0;
+        }
+        return array;
+    }
 }
