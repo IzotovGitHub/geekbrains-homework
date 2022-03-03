7f88deb8755fd9f8113c2cb04327820098d19760
izotov <aleksandr.izotov2020@gmail.com>
2022-03-03 22:41:44 +0300

    Added fillInInverseDiagonalOfSquareMatrix method

--- src/geek/brains/quarter/one/java1/lession3/SquareMatrix.java
+++ src/geek/brains/quarter/one/java1/lession3/SquareMatrix.java
@@ -2,6 +2,21 @@ package geek.brains.quarter.one.java1.lession3;
 
 public class SquareMatrix {
 
+    private static int[][] fillInInverseDiagonalOfSquareMatrix(int[][] matrix, int value) {
+        if (isSquareMatrix(matrix)) {
+            int len = matrix.length;
+            for (int i = 0; i < len; i++){
+                for(int j = 0; j < len; j++){
+                    if(i == Math.abs(len - j - 1)){
+                        matrix[i][j] = value;
+                        break;
+                    }
+                }
+            }
+        }
+        return matrix;
+    }
+
     private static boolean isSquareMatrix(int[][] matrix) {
         int len = matrix.length;
         for (int[] row : matrix) {
