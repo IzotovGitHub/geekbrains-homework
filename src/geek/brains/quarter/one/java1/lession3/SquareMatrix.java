3501377c657f36a60f1b7a5af09327bbefc52312
izotov <aleksandr.izotov2020@gmail.com>
2022-03-03 22:38:18 +0300

    Added buildSquareMatrix method

--- src/geek/brains/quarter/one/java1/lession3/SquareMatrix.java
+++ src/geek/brains/quarter/one/java1/lession3/SquareMatrix.java
@@ -2,4 +2,11 @@ package geek.brains.quarter.one.java1.lession3;
 
 public class SquareMatrix {
 
+    private static int[][] buildSquareMatrix(int order) {
+        int[][] matrix = new int[order][];
+        for (int i = 0; i < order; i++) {
+            matrix[i] = new int[order];
+        }
+        return matrix;
+    }
 }
