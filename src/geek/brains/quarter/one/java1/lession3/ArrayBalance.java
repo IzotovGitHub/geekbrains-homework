9910f78c17cfb76729d2f6fc7ce51d83eb4b160c
izotov <aleksandr.izotov2020@gmail.com>
2022-03-04 18:05:42 +0300

    Added arrayRangeSum method

--- src/geek/brains/quarter/one/java1/lession3/ArrayBalance.java
+++ src/geek/brains/quarter/one/java1/lession3/ArrayBalance.java
@@ -2,4 +2,18 @@ package geek.brains.quarter.one.java1.lession3;
 
 public class ArrayBalance {
 
+    private static int arrayRangeSum(int[] array, int leftBorder, int rightBorder) {
+        if (leftBorder >= 0 && leftBorder <= rightBorder && rightBorder < array.length) {
+            if (leftBorder == rightBorder) {
+                return array[leftBorder];
+            }
+
+            int sum = 0;
+            while (leftBorder <= rightBorder) {
+                sum += array[leftBorder++];
+            }
+            return sum;
+        }
+        return 0;
+    }
 }
