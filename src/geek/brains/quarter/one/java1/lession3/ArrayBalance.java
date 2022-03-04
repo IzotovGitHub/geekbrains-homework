bb7e74304a15e25918dccfc03fe8783eb84e5ade
izotov <aleksandr.izotov2020@gmail.com>
2022-03-04 18:06:14 +0300

    Added checkBalance method

--- src/geek/brains/quarter/one/java1/lession3/ArrayBalance.java
+++ src/geek/brains/quarter/one/java1/lession3/ArrayBalance.java
@@ -2,6 +2,19 @@ package geek.brains.quarter.one.java1.lession3;
 
 public class ArrayBalance {
 
+    private static boolean checkBalance(int[] array) {
+        int lastIndex = array.length - 1;
+        for (int i = 0; i < lastIndex; i++) {
+            int leftWeight = arrayRangeSum(array, 0, i);
+            int rightWeight = arrayRangeSum(array, i + 1, lastIndex);
+
+            if (leftWeight == rightWeight) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private static int arrayRangeSum(int[] array, int leftBorder, int rightBorder) {
         if (leftBorder >= 0 && leftBorder <= rightBorder && rightBorder < array.length) {
             if (leftBorder == rightBorder) {
