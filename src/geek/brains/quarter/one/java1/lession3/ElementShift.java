09cb5a76c019b597a11fbdc6681d2c4ec8eefc79
izotov <aleksandr.izotov2020@gmail.com>
2022-03-04 20:00:07 +0300

    Added toRight method

--- src/geek/brains/quarter/one/java1/lession3/ElementShift.java
+++ src/geek/brains/quarter/one/java1/lession3/ElementShift.java
@@ -1,4 +1,16 @@
 package geek.brains.quarter.one.java1.lession3;
 
 public class ElementShift {
+
+    private static int[] toRight(int[] array) {
+        int len = array.length;
+        int cache = array[len - 1];
+
+        for (int i = len - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+
+        array[0] = cache;
+        return array;
+    }
 }
