719215c83af08583c4d6e61694c7cfa9f9709419
izotov <aleksandr.izotov2020@gmail.com>
2022-03-26 22:13:20 +0300

    Added Animal class

---
+++ src/geek/brains/quarter/one/java1/lession6/Animal.java
@@ -0,0 +1,31 @@
+package geek.brains.quarter.one.java1.lession6;
+
+import static java.lang.String.format;
+
+public class Animal {
+
+    private static int animals = 0;
+
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+        animals ++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void swim(int distance){
+        System.out.printf("%s проплыл %s м%n", name, distance);
+    }
+
+    public void run(int distance){
+        System.out.printf("%s пробежал %s м%n", name, distance);
+    }
+
+    public int getAnimals() {
+        return animals;
+    }
+}
