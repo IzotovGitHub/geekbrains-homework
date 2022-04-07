e67730d5e3752dd3e0853810de103b10d265a044
izotov <aleksandr.izotov2020@gmail.com>
2022-04-07 11:13:39 +0300

    Added HomeWorkApp class

---
+++ src/geek/brains/quarter/one/java1/lession7/HomeWorkApp.java
@@ -0,0 +1,27 @@
+package geek.brains.quarter.one.java1.lession7;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        Cat[] cats = new Cat[]{
+                new Cat("Max", 10),
+                new Cat("Barsik", 5),
+                new Cat("Murzik", 15)
+        };
+
+        Plate plate = new Plate(20);
+
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            System.out.printf("Кот %s статус сытости %s %n", cat.getName(), cat.isSatiety());
+        }
+
+        System.out.println();
+
+        plate.addFood(10);
+        for (Cat cat : cats) {
+            cat.eat(plate);
+            System.out.printf("Кот %s статус сытости %s %n", cat.getName(), cat.isSatiety());
+        }
+    }
+}
