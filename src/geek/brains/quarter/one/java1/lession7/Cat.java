1fe966119f31437910d46a5ab0f0a32159b26d92
izotov <aleksandr.izotov2020@gmail.com>
2022-04-07 11:13:24 +0300

    Added Cat class

---
+++ src/geek/brains/quarter/one/java1/lession7/Cat.java
@@ -0,0 +1,55 @@
+package geek.brains.quarter.one.java1.lession7;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+
+    public Cat(String name) {
+        this(name, 50);
+    }
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate plate) {
+        if (appetite > 0) {
+            int foodQuantity;
+            if (plate.isFoodEnough(appetite)) {
+                System.out.printf("%s начал есть, съел %d %n", this.name, appetite);
+                plate.decreaseFood(this, appetite);
+                appetite = 0;
+            } else if ((foodQuantity = plate.getFoodQuantity()) > 0) {
+                System.out.printf("%s начал есть. Доел всю еду в миске (%d), но остался голодным %n", this.name, foodQuantity);
+                plate.decreaseFood(this, foodQuantity);
+                appetite -= foodQuantity;
+            } else {
+                System.out.println("В миске нет еды.");
+            }
+        } else {
+            System.out.printf("%s не голоден %n", this.name);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public void setAppetite(int appetite) {
+        this.appetite = appetite;
+    }
+
+    public boolean isSatiety() {
+        return appetite == 0;
+    }
+}
