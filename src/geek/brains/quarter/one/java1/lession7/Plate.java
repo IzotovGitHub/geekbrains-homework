13e3e12902515f30be07d53f6930a25ac2691f35
izotov <aleksandr.izotov2020@gmail.com>
2022-04-07 11:13:13 +0300

    Added Plate class

---
+++ src/geek/brains/quarter/one/java1/lession7/Plate.java
@@ -0,0 +1,34 @@
+package geek.brains.quarter.one.java1.lession7;
+
+public class Plate {
+
+    private int foodQuantity;
+
+    public Plate(int foodQuantity) {
+        this.foodQuantity = foodQuantity;
+    }
+
+    public void info() {
+        System.out.println("Текущее кол-во еды " + foodQuantity);
+    }
+
+    public void decreaseFood(Cat cat, int foodQuantity) {
+        if (this.foodQuantity >= foodQuantity) {
+            this.foodQuantity -= foodQuantity;
+        } else {
+            this.foodQuantity = 0;
+        }
+    }
+
+    public void addFood(int foodQuantity) {
+        this.foodQuantity += foodQuantity;
+    }
+
+    public int getFoodQuantity() {
+        return foodQuantity;
+    }
+
+    public boolean isFoodEnough(int foodCount) {
+        return this.foodQuantity >= foodCount;
+    }
+}
