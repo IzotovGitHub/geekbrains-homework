73cdf14eabcb03ddfad4a695266c918d26aaa200
izotov <aleksandr.izotov2020@gmail.com>
2022-04-13 21:47:53 +0300

    Added lesson 2 homework

---
+++ src/geek/brains/quarter/one/java2/lesson2/exception/MyArrayDataException.java
@@ -0,0 +1,22 @@
+package geek.brains.quarter.one.java2.lesson2.exception;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException() {
+    }
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+
+    public MyArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyArrayDataException(Throwable cause) {
+        super(cause);
+    }
+
+    public MyArrayDataException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
